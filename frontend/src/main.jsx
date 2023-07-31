177803daa1d0c8f08e73785218404cd5733ea530
seyrio <agrawal3h2@gmail.com>
2023-08-01 03:07:22 +0530

    added profile functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import HomeScreen from './screens/HomeScreen.jsx'
 import LoginScreen from './screens/LoginScreen.jsx'
 import RegisterScreen from './screens/RegisterScreen.jsx'
+import ProfileScreen from './screens/ProfileScreen.jsx';
+import PrivateRoute from './components/PrivateRoute.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -15,7 +17,9 @@ const router = createBrowserRouter(
       <Route index={true} path='/' element={<HomeScreen/>}/>
       <Route path='/login' element={<LoginScreen/>}/>
       <Route path='/register' element={<RegisterScreen/>}/>
-
+      <Route path='' element={<PrivateRoute />}>
+      <Route path='/profile' element={<ProfileScreen />} />
+      </Route>
     </Route>
   )
 )
