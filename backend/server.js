27a3c5752fa9f3e409b0766cbf75b1ccc3d1546f
seyrio <agrawal3h2@gmail.com>
2023-07-31 14:52:45 +0530

    initialize routes controllers and database

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+dotenv.config();
+
+connectDB();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use('/api/users', userRoutes);
+app.use(notFound);
+app.use(errorHandler);
+app.get('/', (req,res)=>res.send('Server is ready'));
+
+app.listen(port, ()=>console.log(`Server started on port ${port}`));
