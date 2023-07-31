a5e15f99d23db6ac594415fd33776a8448cf9965
seyrio <agrawal3h2@gmail.com>
2023-07-31 18:56:11 +0530

    added register authentication and logout methods

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,24 +1,68 @@
 import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
+import User from "../models/userModel.js";
 
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Auth User'});
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+
+    if(user && (await user.matchPassword(password))) {
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
 });
 
 // @desc Register a new user
 // route POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Register User'});
+    const {name, email, password} = req.body;
+    const userExists = await User.findOne({email});
+
+    if(userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if(user) {
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
 });
 
 // @desc Logout user
 // route POST /api/users/logout
 // @access Public
 const logoutUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Logout User'});
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+
+    res.status(200).json({message: "User logged Out"})
 });
 
 // @desc Get user profile
