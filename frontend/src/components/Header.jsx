254065d9e082f2a4af4cce9805dbcdb1956ae9a1
seyrio <agrawal3h2@gmail.com>
2023-08-01 02:56:44 +0530

    register and login functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,30 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import { logout } from '../slices/authSlice';
 
 const Header = () => {
+   const { userInfo } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/login');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -13,6 +35,19 @@ const Header = () => {
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
             <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
+            {userInfo ? (
+                <>
+                  <NavDropdown title={userInfo.name} id='username'>
+                    <LinkContainer to='/profile'>
+                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              ) : (
+                <>
                 <LinkContainer to='/login'>
                     <Nav.Link>
                         <FaSignInAlt /> Sign In
@@ -23,6 +58,7 @@ const Header = () => {
                         <FaSignOutAlt /> Sign Up
                     </Nav.Link>
                 </LinkContainer>
+                </> )}
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -1,7 +1,13 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import FormContainer from '../components/FormContainer';
+import Loader from '../components/Loader';
+// import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useRegisterMutation } from '../slices/usersApiSlice';
+import { setCredentials } from '../slices/authSlice';
+import { toast } from 'react-toastify';
 
 const RegisterScreen = () => {
     const [name, setName] = useState('');
@@ -9,9 +15,33 @@ const RegisterScreen = () => {
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+  
+    const [register, { isLoading }] = useRegisterMutation();
+  
+    const { userInfo } = useSelector((state) => state.auth);
+
+    useEffect(() => {
+      if (userInfo) {
+        navigate('/');
+      }
+    }, [navigate, userInfo]);
+  
+
     const submitHandler = async (e) => {
         e.preventDefault();
-        console.log('submit');
+        if (password !== confirmPassword) {
+          toast.error('Passwords do not match');
+        } else {
+          try {
+            const res = await register({ name, email, password }).unwrap();
+            dispatch(setCredentials({ ...res }));
+            navigate('/');
+          } catch (err) {
+            toast.error(err?.data?.message || err.error);
+          }
+        }
       };
 
   return (
@@ -66,6 +96,8 @@ const RegisterScreen = () => {
         >
           Sign In
         </Button>
+
+        {isLoading && <Loader />}
       </Form>
 
       <Row className='py-3'>
