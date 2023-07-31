abc170d3ae1f3de8e99f9d0ae084bd73924704c3
seyrio <agrawal3h2@gmail.com>
2023-07-31 22:48:51 +0530

    frontend ui

--- frontend/screens/HomeScreen.jsx
+++
@@ -1,7 +0,0 @@
-import Hero from "../src/components/Hero"
-
-const HomeScreen = () => {
-  return <Hero/>;
-}
-
-export default HomeScreen
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
 import { Container } from "react-bootstrap"
-import Header from "./components/header"
+import Header from "./components/Header"
 
 const App = () => {
   return (
